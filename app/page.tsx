d101a80e393265a2633bffc476314b5777bddb50
Emmanuek5 <emmanuelandrew625@yahoo.com>
2024-12-18 03:29:02 +0100

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -83,8 +83,8 @@ export default function Home() {
     if (!currentUser || !sdk?.channelId) return;
 
     const connectSocket = async () => {
-      const newSocket = io("/socket", {
-        path: "./proxy/socket",
+      const newSocket = io("./proxy/https://socket.devit.lol/socket.io", {
+        // path: "socket",
         transports: ["polling", "websocket"],
         timeout: 5000,
         extraHeaders: {
